3d2b69c28196afc2d4a54356aaaf5abaffcb1c7a
VincentZimbiziAngel <v.zimbizi@gmail.com>
2023-07-26 01:45:46 +0300

    OnlineShop

---
+++ src/Customer.java
@@ -0,0 +1,52 @@
+public class Customer {
+
+    private String lastName;
+    private String firstName;
+    private String sex;
+    private String birthDate;
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getSex() {
+        return this.sex;
+    }
+
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public static void main(String[] args) {
+        Customer c = new Customer();
+        c.setLastName("Escobar");
+        c.setFirstName("Pablo");
+        c.setSex("Male");
+        c.setBirthDate("1/12/1949");
+
+        System.out.println(c.getLastName());
+        System.out.println(c.getFirstName());
+        System.out.println(c.getSex());
+        System.out.println(c.getBirthDate());
+    }
+}
