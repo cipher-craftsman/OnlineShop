8d12cbc24a8c964e81ff298eccb4b8d7bcf51092
VincentZimbiziAngel <v.zimbizi@gmail.com>
2024-06-17 16:51:50 -0500

    yess

--- src/Customer.java
+++ src/Customer.java
@@ -4,6 +4,9 @@ public class Customer {
     private String firstName;
     private String sex;
     private String birthDate;
+    private String birthPlace;
+    private String address;
+    private Float height;
 
     public String getLastName() {
         return this.lastName;
@@ -19,6 +22,18 @@ public class Customer {
 
     public String getBirthDate() {
         return this.birthDate;
+    }
+
+    public String getBirthPlace() {
+        return this.birthPlace;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public Float getHeight() {
+        return this.height;
     }
 
     public void setLastName(String lastName) {
@@ -37,18 +52,34 @@ public class Customer {
         this.birthDate = birthDate;
     }
 
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+
     public static void main(String[] args) {
         Customer c = new Customer();
         c.setLastName("Escobar");
         c.setFirstName("Pablo");
         c.setSex("Male");
         c.setBirthDate("1/12/1949");
+        c.setBirthPlace("Colombia");
+        c.setAddress("Medellin, Colombia");
+        c.setHeight(1.75f); 
 
         System.out.println(c.getLastName());
         System.out.println(c.getFirstName());
         System.out.println(c.getSex());
         System.out.println(c.getBirthDate());
-     
-         
+        System.out.println(c.getBirthPlace());
+        System.out.println(c.getHeight());
+        
     }
 }
