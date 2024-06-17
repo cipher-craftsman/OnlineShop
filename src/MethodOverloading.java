338a2623fe0462018a0e5a3d35dec6ce32a0e230
VincentZimbiziAngel <v.zimbizi@gmail.com>
2024-06-17 16:51:23 -0500

    yes

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -4,14 +4,18 @@ public class MethodOverloading {
         int result1 = addNumbers(5, 2);
         double result2 = addNumbers(5.2, 2.1);
         int result3 = addNumbers(5, 2, 3);
+        Float result4 = (float) divideNumbers(7, 3);
+        Float result5 = (float) multiplyNumbers(5,5);
 
         System.out.println(result1);
         System.out.println(result2);
         System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
     }
 
     public static int addNumbers(int a, int b) {
-        return a + b;
+        return a + b; 
     }
 
     public static double addNumbers(double a, double b) {
@@ -22,4 +26,13 @@ public class MethodOverloading {
     public static int addNumbers(int a, int b, int c) {
         return a + b + c;
     }
+
+    public static int divideNumbers(int i, int j) {
+        return i/j;
+    }
+
+    public static float multiplyNumbers(int x, int y) {
+        return x*y;
+    }
 }
+
